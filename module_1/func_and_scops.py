fd453c43c3a7c05366fac0eff8cea61c195106c6
Роман Турсков <roman.turskov@yandex.ru>
2025-10-22 15:24:08 +0300

    feat(TASK-839): Modules and Packages (module_1/main.py, module_1/apps/utils)

--- module_1/func_and_scops.py
+++ module_1/func_and_scops.py
@@ -1,6 +1,6 @@
 def logger(message, *args, **kwargs):
     x = args
-    y = {k:v for k, v in kwargs.items()}
+    y = {k: v for k, v in kwargs.items()}
     print(f'{message}\n'
           f'{x}\n'
           f'{y}')
---
+++ module_1/main.py
@@ -0,0 +1,11 @@
+from apps.utils import logger
+
+
+if __name__ == "__main__":
+    mes = "Hello world"
+    a = 2
+    b = 3
+    c = 4
+
+    logger(mes, a, b, c, {'1': 'a', '2': 'b', '3': 'c'}, v=6)
+    logger("Test", 1, 2, user="admin")
