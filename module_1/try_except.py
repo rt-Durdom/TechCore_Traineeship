0fd4bbcf799c1ea1b17f90ead1bcb99d9d01a677
Роман Турсков <roman.turskov@yandex.ru>
2025-10-22 17:54:22 +0300

    feat(TASK-839): Try - except, Exception)

---
+++ module_1/try_except.py
@@ -0,0 +1,21 @@
+import logging
+
+
+def open_file():
+    try:
+        with open("module_1/data_txt/data.txt", "r") as file:
+            print(file.read())
+            logging.info("Файл успешно открыт.")
+    except FileNotFoundError as not_f:
+        logging.error(not_f)
+        raise FileNotFoundError("Файл не найден.")
+    except PermissionError as perm:
+        logging.error(perm)
+        raise PermissionError("Нет прав на чтение.")
+    except Exception as ex:
+        logging.error(ex)
+        raise Exception("Что-то пошло не так.")
+
+
+if __name__ == "__main__":
+    open_file()
