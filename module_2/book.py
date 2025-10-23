e2d9c160cb44ffcb262ef4e35b8a3e58a27125e2
Роман Турсков <roman.turskov@yandex.ru>
2025-10-23 11:55:07 +0300

    feat(TASK-841): Book class (book.py)

--- module_2/book.py
+++ module_2/book.py
@@ -1,11 +1,3 @@
-# Задача: Классы и __init__
-
-# Цель: Реализовать свой первый класс.
-
-# Задание: Создать класс Book с __init__(self, title, author).
-
-# Критерии приемки: Класс создан.
-
 class Book:
     def __init__(self, title, author):
         self.title = title
