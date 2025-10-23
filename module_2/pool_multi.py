128b23777343677e4e2a1e28d0d0ddb6805d081d
Роман Турсков <roman.turskov@yandex.ru>
2025-10-23 16:05:24 +0300

    feat(TASK-841): multiprocessing.Pool (pool_multi.py)

--- module_2/pool_multi.py
+++ module_2/pool_multi.py
@@ -1,17 +1,17 @@
 import multiprocessing
 
 
-def heavy_task():
+def heavy_task(_):
     return sum(range(100_000_000))
 
 
 if __name__ == "__main__":
-    tasks = list(range(1, 11))
+    tasks_list = list(range(1, 11))
 
-    cpu_count = multiprocessing.cpu_count()
+    count_core = multiprocessing.cpu_count()
 
-    with multiprocessing.Pool(processes=cpu_count) as pool:
-        results = pool.map(heavy_task, tasks)
-    print(f'Ядер процессора задействовано: {cpu_count}')
-    for result in results:
-        print(result)
+    pool = multiprocessing.Pool(processes=count_core)
+    res = list(pool.map(heavy_task, tasks_list))
+
+    print(f'Ядер процессора задействовано: {count_core}')
+    print(*res)
