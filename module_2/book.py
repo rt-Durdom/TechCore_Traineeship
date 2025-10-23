f4a113e4a3741d480e3128ed2e6219c6e514649f
Роман Турсков <roman.turskov@yandex.ru>
2025-10-23 12:02:57 +0300

    feat(TASK-841): magik method __repr__, __eq__ (book.py)

--- module_2/book.py
+++ module_2/book.py
@@ -2,3 +2,14 @@ class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}: "{self.title}", "{self.author}"'
+
+    def __eq__(self, other):
+        return self.title == other.title and self.author == other.author
+
+book1 = Book('Война и мир', 'Лев Толстой')
+book2 = Book('Война и мир', 'Лев Толстой')
+print(book1 == book2)
+#True
