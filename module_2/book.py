5d5b4319ef4553307b6d8be15a59cda1f02460cb
Роман Турсков <roman.turskov@yandex.ru>
2025-10-23 12:11:49 +0300

    feat(TASK-841): Inheritance Ebook (book.py)

--- module_2/book.py
+++ module_2/book.py
@@ -9,7 +9,24 @@ class Book:
     def __eq__(self, other):
         return self.title == other.title and self.author == other.author
 
+class Ebook(Book):
+    def __init__(self, title, author, file_size):
+        super().__init__(title, author)
+        self.file_size = file_size
+
+    def __repr__(self):
+        return (
+            f'{self.__class__.__name__}:'
+            f' "{self.title}", "{self.author}",'
+            f' Размер файла: {self.file_size}'
+        )
+
+
 book1 = Book('Война и мир', 'Лев Толстой')
 book2 = Book('Война и мир', 'Лев Толстой')
 print(book1 == book2)
 #True
+
+book3 = Ebook('Война и мир', 'Лев Толстой', 100)
+print(book3)
+
