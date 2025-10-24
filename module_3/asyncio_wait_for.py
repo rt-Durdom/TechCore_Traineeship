f8acce29db37600619c8e09ac7a70232c5c6e634
Роман Турсков <roman.turskov@yandex.ru>
2025-10-24 10:27:50 +0300

    feat(TASK-843): asyncio.wait_for (asyncio_wait_for.py)

---
+++ module_3/asyncio_wait_for.py
@@ -0,0 +1,18 @@
+import asyncio
+
+
+async def eternity():
+    await asyncio.sleep(5)
+    print('Програма сна заверешна!')
+
+
+
+async def main():
+    try:
+        await asyncio.wait_for(eternity(), timeout=2)
+    except TimeoutError:
+        print('Досросчно завершили сон!')
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
