0f6b2cdd56fa4d1b8fc81b55f5120e3daa608fc1
Роман Турсков <roman.turskov@yandex.ru>
2025-10-28 15:54:43 +0300

    feat(TASK-845): Асинхронные эндпоинты Exceptions

--- module_4/app/main.py
+++ module_4/app/main.py
@@ -3,32 +3,8 @@ from fastapi.exceptions import HTTPException
 
 from .schemas.books import BookSchema
 from .core.db import get_db_session, Session
+from .api.routers import api_router
 
 app = FastAPI()
 
-book_dict = dict()
-id = 1
-
-
-@app.get('/')
-def read_root():
-    return {"Hello": "World"}
-
-@app.post('/books')
-def create_book(book: BookSchema):
-    if book_dict[id]:
-        id += 1
-        book_dict[id] = book
-    book_dict[id] = book
-    return f'Под номером {id} книга {book.title} создана'
-
-@app.get('/books/{book_id}')
-def read_book(book_id: int):
-    if book_id not in book_dict:
-        raise HTTPException(status_code=404, detail='Книга не найдена')
-    return f'Книга {book_dict[book_id].title}'
-
-@app.post('/books_id')
-async def create_book(book: BookSchema, db: Session = Depends(get_db_session)):
-    result = db.responce()
-    return {'mes': result}
+app.include_router(api_router)
