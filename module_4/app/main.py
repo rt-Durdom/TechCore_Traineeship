97d64719a68155f1c37d7bc0b552d9c7f05769ab
Роман Турсков <roman.turskov@yandex.ru>
2025-10-28 16:09:41 +0300

    feat(TASK-845): Middleware (Логирование)

--- module_4/app/main.py
+++ module_4/app/main.py
@@ -1,10 +1,18 @@
-from fastapi import FastAPI, Depends
-from fastapi.exceptions import HTTPException
+import time
 
-from .schemas.books import BookSchema
-from .core.db import get_db_session, Session
+from fastapi import FastAPI, Request
+
 from .api.routers import api_router
 
 app = FastAPI()
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    method = request.method
+    url = request.url
+    start_time = time.perf_counter()
+    await call_next(request)
+    process_time = time.perf_counter() - start_time
+    return f'Информация о запросе: {method}, {url}, {process_time}'
 
 app.include_router(api_router)
