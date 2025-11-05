402f17373223c379d92f745d476ab969cdc2a06d
Роман Турсков <roman.turskov@yandex.ru>
2025-11-05 14:14:39 +0300

    feat(TASK-847): Отношения (Relationships)

--- module_4/app/models/authors.py
+++ module_4/app/models/authors.py
@@ -1,5 +1,5 @@
 from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base
 
@@ -7,3 +7,6 @@ from .base import Base
 class Author(Base):
     name: Mapped[str] = mapped_column(String(100), nullable=False)
     descript: Mapped[str] = mapped_column(String(100), nullable=True)
+
+    #relationship
+    books: Mapped[list['Book']] = relationship('Book', back_populates='author')
--- module_4/app/models/books.py
+++ module_4/app/models/books.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Integer, String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Integer, String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base
 
@@ -8,3 +8,7 @@ class Book(Base):
 
     title: Mapped[str] = mapped_column(String(100), nullable=False)
     year: Mapped[int] = mapped_column(Integer, nullable=True)
+    author_id: Mapped[int] = mapped_column(ForeignKey('author.id'), nullable=True)
+
+    #relationships
+    author: Mapped['Author'] = relationship('Author', back_populates='books')
