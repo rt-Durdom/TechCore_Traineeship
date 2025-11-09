fee744e1b3a3e91bed100681ba582655be7f80c6
Роман Турсков <roman.turskov@yandex.ru>
2025-11-09 17:27:29 +0300

    feat(TASK-851): Тестирование (Pytest)

---
+++ tests/test_first.py
@@ -0,0 +1,14 @@
+def multi(a, b):
+    return a * b
+
+
+def test_multi():
+    assert multi(2, 3) == 6
+
+
+def summm(a, b):
+    return a + b
+
+
+def test_simple_add():
+    assert summm(1, 1) == 2
