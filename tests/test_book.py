13f9d3a9a6b35d636a27344e28f1c2f626f3c362
Роман Турсков <roman.turskov@yandex.ru>
2025-11-10 20:21:01 +0300

    feature(TASK-851): pytest-asyncio

--- tests/test_book.py
+++ tests/test_book.py
@@ -9,17 +9,18 @@ from module_4.app.core.db import get_db_session
 from module_4.app.main import app
 
 
+#pytest-asyncio
 @pytest.mark.asyncio
 async def test_mock_get_book(mocker):
     book_service = CRUDAsyncBase(Book)
     test_book = Book(id=5, title='Тест', author='Пушукин', year=1812)
-    mock_session = mocker.AsyncMock()
+    mock_session = mocker.AsyncMock(spec=AsyncSession)
     mocket_data = mocker.patch.object(
         book_service,
         'get_obj_by_id',
         AsyncMock(return_value=test_book)
     )
-    result = await book_service.get_obj_by_id(5, None)
+    result = await book_service.get_obj_by_id(5, mock_session)
     assert result.id == 5
     assert result.title == 'Тест'
     assert result.author == 'Пушукин'
