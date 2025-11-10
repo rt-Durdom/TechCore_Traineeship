f77d40453c46719b40b11cbdbb6e9b1f0a5bf592
Роман Турсков <roman.turskov@yandex.ru>
2025-11-10 18:59:23 +0300

    feature(TASK-851): FastAPI TestClient (Интеграция)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,12 @@
 import pytest
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+from fastapi.testclient import TestClient
+from module_4.app.main import app
 
 engine = create_engine("sqlite:///:memory:")
 Base_QL = declarative_base()
+fast_test_client = TestClient(app)
 
 @pytest.fixture
 def db_session():
--- tests/test_book.py
+++ tests/test_book.py
@@ -2,12 +2,13 @@ import pytest
 from unittest.mock import AsyncMock
 
 from module_4.app.models.async_crud import CRUDAsyncBase
-from module_4.app.models import Book    
+from module_4.app.models import Book
+from .conftest import fast_test_client
 
 
 @pytest.mark.asyncio
 async def test_mock_get_book(mocker):
-    book_service = CRUDAsyncBase(model=Book)
+    book_service = CRUDAsyncBase(Book)
     test_book = Book(id=5, title='Тест', author='Пушукин', year=1812)
     mock_session = mocker.Mock()
     mocket_data = mocker.patch.object(
@@ -23,3 +24,8 @@ async def test_mock_get_book(mocker):
     assert test_book == result
 
     mocket_data.assert_called_once_with(5, mock_session)
+
+
+def test_get_book_endpoint():
+    response = fast_test_client.get('/books/books')
+    assert response.status_code == 200
