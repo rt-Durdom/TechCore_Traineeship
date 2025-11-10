e177a3395c2098ab0290f67025bddd82d6bd1169
Роман Турсков <roman.turskov@yandex.ru>
2025-11-10 21:38:33 +0300

    fix(TASK-851): некоторые баги

---
+++ module_4/tests/test_book.py
@@ -0,0 +1,64 @@
+import pytest
+import httpx
+from unittest.mock import AsyncMock
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from module_4.app.models.async_crud import CRUDAsyncBase
+from module_4.app.models import Book
+from module_4.app.core.db import get_db_session
+from .conftest import fast_test_client
+
+
+
+
+
+#pytest-asyncio
+@pytest.mark.asyncio
+async def test_mock_get_book(mocker):
+    book_service = CRUDAsyncBase(Book)
+    test_book = {'id': 5, 'title': 'Тест', 'author': 'Пушукин', 'year': 1812}
+    mock_session = mocker.AsyncMock(spec=AsyncSession)
+    mocket_data = mocker.patch.object(
+        book_service,
+        'get_obj_by_id',
+        AsyncMock(return_value=test_book)
+    )
+    result = await book_service.get_obj_by_id(5, mock_session)
+    assert result['id'] == 5
+    assert result['title'] == 'Тест'
+    assert result['author'] == 'Пушукин'
+    assert result['year'] == 1812
+    assert test_book == result
+
+    mocket_data.assert_called_once_with(5, mock_session)
+
+
+def test_get_book_endpoint():
+    response = fast_test_client.get('/books')
+    assert response.status_code == 200
+
+
+def test_get_book_endpoint_sesion():
+    mock_session = AsyncMock(spec=AsyncSession)
+    app.dependency_overrides[get_db_session] = lambda: mock_session
+    try:
+        response = fast_test_client.get('/books')
+        assert response.status_code == 200
+    finally:
+        app.dependency_overrides.clear()
+
+
+def test_post_book_pydantic():
+    response = fast_test_client.post(
+        '/books/books',
+        json={'title': 123, 'author': "Пушукин", 'year': 1812}
+    )
+    assert response.status_code == 422
+
+
+@pytest.mark.asyncio
+async def test_async_get_book():
+    async with httpx.AsyncClient(app=app, base_url="http://test") as client:
+        response = await client.get('/books')
+
+    assert response.status_code == 200
