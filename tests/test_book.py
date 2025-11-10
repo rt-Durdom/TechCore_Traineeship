dcb30890f761f7cebb95df23ae1d7091b22d05d2
Роман Турсков <roman.turskov@yandex.ru>
2025-11-10 18:39:00 +0300

    feature(TASK-851): Мокинг (pytest-mock) mocked

---
+++ tests/test_book.py
@@ -0,0 +1,25 @@
+import pytest
+from unittest.mock import AsyncMock
+
+from module_4.app.models.async_crud import CRUDAsyncBase
+from module_4.app.models import Book    
+
+
+@pytest.mark.asyncio
+async def test_mock_get_book(mocker):
+    book_service = CRUDAsyncBase(model=Book)
+    test_book = Book(id=5, title='Тест', author='Пушукин', year=1812)
+    mock_session = mocker.Mock()
+    mocket_data = mocker.patch.object(
+        book_service,
+        'get_obj_by_id',
+        AsyncMock(return_value=test_book)
+    )
+    result = await book_service.get_obj_by_id(5, None)
+    assert result.id == 5
+    assert result.title == 'Тест'
+    assert result.author == 'Пушукин'
+    assert result.year == 1812
+    assert test_book == result
+
+    mocket_data.assert_called_once_with(5, mock_session)
