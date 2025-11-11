16ac2bb15c673d18f65ec28d4be669a70b07a9f9
Роман Турсков <roman.turskov@yandex.ru>
2025-11-11 14:46:03 +0300

    feature(TASK-853): asyncio.wait_for (Таймауты)/Backoff (Retry Policy)

--- module_8/async_core/AsyncClient_service.py
+++ module_8/async_core/AsyncClient_service.py
@@ -1,11 +1,21 @@
-from httpx import AsyncClient
+import asyncio
+import backoff
+import httpx
 
 
 class AuthorService:
-    def __init__(self, base_url: str =  None):
+    def __init__(self, base_url: str | None = None):
         self.base_url = base_url
-        self.client = AsyncClient()
+        self.client = httpx.AsyncClient()
 
+    @backoff.on_exception(backoff.expo, httpx.RequestError, max_tries=3)
     async def get_data(self):
-        response = await self.client.get(self.base_url)
-        return response
+        try:
+            response = await asyncio.wait_for(await self.client.get(self.base_url), timeout=2.0)
+            return response
+        except asyncio.TimeoutError as e:
+            raise TimeoutError(f'Время ожидания истекл. Ошибка {e}')
+        except Exception as e:
+            raise Exception(f'Что-то пошло не так {e}')
+        finally:
+            await self.client.aclose
