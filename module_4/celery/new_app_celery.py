5486ff6d9f846f50d3f671bd85e68afaecf4b9fc
Роман Турсков <roman.turskov@yandex.ru>
2025-11-19 14:42:34 +0300

    feature(TASK-855): Настройка Celery

---
+++ module_4/celery/new_app_celery.py
@@ -0,0 +1,9 @@
+from celery import Celery
+
+
+celery_app = Celery(
+    'worker_celery',
+    broker='amqp://guest:guest@localhost:5672//',
+    backend='redis://localhost:6379/0',
+    include=['celery.tasks']
+)
