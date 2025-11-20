e720e0f943c4dbe36d8fbf13faf9582150d29983
Роман Турсков <roman.turskov@yandex.ru>
2025-11-20 15:07:06 +0300

    feature(TASK-855): Dead Letter Queue (DLQ)

--- module_4/app_celery/new_app_celery.py
+++ module_4/app_celery/new_app_celery.py
@@ -1,4 +1,5 @@
 from celery import Celery
+from kombu import Queue
 
 
 celery_app = Celery(
@@ -6,4 +7,18 @@ celery_app = Celery(
     broker='amqp://guest:guest@localhost:5672//',
     backend='redis://localhost:6379/0',
     include=['module_4.app_celery.tasks']
+)
+
+celery_app.conf.update(
+    task_acks_late=True,
+    task_reject_on_worker_lost=True,
+    task_queues=(
+        Queue(
+            'celery',
+            queue_arguments={
+                'x-dead-letter-exchange': '',
+                'x-dead-letter-routing-key': 'celery_error'
+            }
+        )
+    )
 )
